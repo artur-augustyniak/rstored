4453759b50c653f1c151c6b5a8ed9f1ca1076204
Artur Augustyniak <artur@aaugustyniak.pl>
2017-05-05 16:03:40 +0200

    worker subthreads join

--- src/base/worker.rs
+++ src/base/worker.rs
@@ -7,17 +7,19 @@
 extern crate syslog;
 
 use ::probing::Probe;
-use std::thread::{spawn, sleep};
+use std::thread::{spawn, sleep, JoinHandle};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Duration;
-use std::sync::{Arc};
+use std::sync::{Arc, Mutex};
 use ::logging::{Logger};
 use ::base::{Config};
 use logging::logger::syslog::Severity as Severity;
+
 static CPU_ANTI_HOG_MILLIS_OFFSET: u64 = 100;
 
 #[derive(Debug)]
 pub struct Worker {
+    th:Arc<Mutex<Vec<JoinHandle<()>>>>,
     logger: Logger,
     ops: Arc<Vec<Box<Probe>>>,
     config: Config,
@@ -31,6 +33,7 @@ impl Worker {
         c: Config
     ) -> Worker {
         Worker {
+            th:Arc::new(Mutex::new(vec![])),
             logger: logger,
             ops: operations,
             config: c,
@@ -40,27 +43,39 @@ impl Worker {
 
 
     pub fn stop(&self) -> () {
+//        let thread_handles = Arc::try_unwrap(self.th.clone());
+//        let  thread_handles = self.th.clone().lock().unwrap();
         self.should_stop.store(true, Ordering::SeqCst);
+
+
+
+//        for handle in &thread_handles {
+//            let _ = handle.join();
+//        }
     }
 
     pub fn start(&self) -> () {
         let a_len = self.ops.len();
+        let mut thread_handles = self.th.lock().unwrap();
+        
         for i in 0..a_len {
             let ops = self.ops.clone();
             let logger = self.logger.clone();
             let timeout = self.config.get_timeout() + CPU_ANTI_HOG_MILLIS_OFFSET;
             let stop_bool = self.should_stop.clone();
-            spawn(move || {
-                loop {
-                    if stop_bool.load(Ordering::SeqCst) {
-                        let msg = format!("@Thread: {} - terminating", ops[i].get_thread_id());
-                        logger.log(Severity::LOG_INFO, &msg);
-                        break
+
+            thread_handles.push(
+                spawn(move || {
+                    loop {
+                        if stop_bool.load(Ordering::SeqCst) {
+                            let msg = format!("@Thread: {} - terminating", ops[i].get_thread_id());
+                            logger.log(Severity::LOG_INFO, &msg);
+                            break
+                        }
+                        ops[i].exec();
+                        sleep(Duration::from_millis(timeout));
                     }
-                    ops[i].exec();
-                    sleep(Duration::from_millis(timeout));
-                }
-            });
+                }));
         }
     }
 }
@@ -68,7 +83,6 @@ impl Worker {
 
 impl Drop for Worker {
     fn drop(&mut self) {
-
         let msg = format!("Worker drop, <free({:?}>)", self);
         self.logger.log(Severity::LOG_INFO, &msg);
     }
